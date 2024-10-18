0b2ff8b73e88983ca801d6ac86812a88345a068d
ryoungj <ruanyangjun@gmail.com>
2024-10-17 20:58:39 -0700

    remove repetitive logging

--- open_lm/logger.py
+++ open_lm/logger.py
@@ -12,6 +12,10 @@ def setup_logging(log_file, level, include_host=False):
         )
     else:
         formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s", datefmt="%Y-%m-%d,%H:%M:%S")
+    
+    # Remove all existing handlers from the root logger
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
 
     logging.root.setLevel(level)
 
